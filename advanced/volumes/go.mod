26d2ae20bff56749c75a2578b1012270d89a495d
chriswilliams1977 <chriswilliams1977@gmail.com>
2020-09-08 10:08:58 +0200

    adding volumes example

---
+++ advanced/volumes/go.mod
@@ -0,0 +1,3 @@
+module github.com/chriswilliams1977/volumes
+
+go 1.13
--- advanced/volumes/main.go
+++ advanced/volumes/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/chriswilliams1977/gomysqlinterfaceexample/models"
+	"github.com/chriswilliams1977/volumes/models"
 	"log"
 	"net/http"
 	"os"
